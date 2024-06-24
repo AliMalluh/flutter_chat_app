45713d06a870ba6a0b0d3c598bc4f25f16fe49b3
Ali Malluh <alimalluh@logestechs.com>
2024-06-24 20:22:56 +0300

    first commit

---
+++ lib/screens/caht.dart
@@ -0,0 +1,42 @@
+import 'package:chat_app/widgets/chat_messages.dart';
+import 'package:chat_app/widgets/new_message.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class CahtScreen extends StatelessWidget {
+  const CahtScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chat'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: Icon(
+              Icons.exit_to_app,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+        ],
+      ),
+      body: const Column(
+        children: [
+          Expanded(
+            child: ChatMessages(),
+          ),
+          SafeArea(
+            top: false,
+            left: false,
+            right: false,
+            bottom: true,
+            child: NewMessage(),
+          ),
+        ],
+      ),
+    );
+  }
+}
